58dae7429b51716b573f8b70f682a99044d424c1
peritrigkas <30832294+peritrigkas@users.noreply.github.com>
2018-12-01 12:17:33 +0000

    Initial commit

--- BDD with Selenium and Specflow/Program.cs
+++ BDD with Selenium and Specflow/Program.cs
@@ -6,16 +6,76 @@ using System.Threading.Tasks;
 
 namespace BDD_with_Selenium_and_Specflow
 {
+    
+    class Triangle
+    {
+        public int triangle_base;
+        public int triangle_height;
+
+        // <access specifier> <name of the construcor> (<parameter>){}
+        // 1. Constructor do not have return type
+        // 2. Name of Constructor should be same as the class
+
+        //public Triangle() //default constructor
+        //{
+        //    Console.WriteLine(" Constructor called");
+        //}
+
+        public Triangle(int a, int b) //parametrised constructor
+        {
+            Console.WriteLine(" Constructor called");
+            triangle_base = a;
+            triangle_height = b;
+        }
+
+
+
+        //<access modifirer> <return type> <name> (<parameter>){}
+
+        public void GetArea()
+        {
+            double area = (0.5 * (triangle_base * triangle_height));
+            Console.WriteLine("Area is : {0}", area);
+        }
+
+        public double GetTriangleArea()
+        {
+            double area = (0.5 * (triangle_base * triangle_height));
+            return area;
+        }
+
+        public void SetValue(int par1, int par2)
+        {
+            triangle_base = par1;
+            triangle_height = par2;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //<class name> <obj name> = new < class name>
+            Triangle triangle1 = new Triangle(10, 20);
+            Triangle triangle2 = new Triangle(100,200);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //triangle1.triangle_base = 10;
+            //triangle1.triangle_height = 20;
+
+            //triangle2.triangle_base = 100;
+            //triangle2.triangle_height = 200;
+
+            //triangle1.GetArea();
+            //triangle2.GetArea();
+
+            triangle1.SetValue(4, 10);
+            triangle2.SetValue(100, 200);
+
+            Console.WriteLine("The Triangle area is : {0}", triangle1.GetTriangleArea());
+            Console.WriteLine("The Triangle area is : {0}", triangle2.GetTriangleArea());
+
+            Console.ReadKey();
         }
+           
     }
 }
